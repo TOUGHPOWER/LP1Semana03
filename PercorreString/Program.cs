889e83f675d491c18a311e1e6dd30fa760399f71
Pedroosorio97 <pedromrnosorio97@gmail.com>
2022-03-21 23:12:06 +0000

    created abd  added PercorreStrings project to solution and completed exercise 1

---
+++ PercorreString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PercorreString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a word or phrase");
+            string inputString = Console.ReadLine();
+            char inputChar = char.Parse(Console.ReadLine());
+            foreach( char c  in inputString)
+            {
+                if(c == inputChar)
+                {
+                    inputString = inputString.Replace(c,char.Parse(" "));
+                }
+                
+            }
+            Console.WriteLine(inputString);
+        }
+    }
+}
