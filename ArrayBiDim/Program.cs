f4049e5cc576eca8b1a00d28cb0bf498531e83d9
Pedroosorio97 <pedromrnosorio97@gmail.com>
2022-03-22 16:49:47 +0000

    Float number storing for each array slot inputed by the user

--- ArrayBiDim/Program.cs
+++ ArrayBiDim/Program.cs
@@ -6,13 +6,24 @@ namespace ArrayBiDim
     {
         static void Main(string[] args)
         {
+            int col, row;
             float[,] biArray ;
-            int col,row;
+            
             Console.Write("Please input number of columns:");
             col = int.Parse(Console.ReadLine());
             Console.Write("Please input number of rows:");
             row = int.Parse(Console.ReadLine()); 
             biArray = new float [col , row];
+            
+            for(int i = 0; i < biArray.GetLength(0);i++)
+            {
+                for(int j = 0; j < biArray.GetLength(0);j++)
+                {
+                    float inputNumber = float.Parse(Console.ReadLine());
+                    
+                }
+                
+            }
             
 
 
